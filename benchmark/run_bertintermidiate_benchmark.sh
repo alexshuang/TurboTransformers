c0322dd8015482e65882e186b3401d20d9613096
Huang Shuang <nikshuang@sina.com>
2020-05-31 01:49:10 +0800

    add bert intermidiate benchmark script

---
+++ benchmark/run_bertintermidiate_benchmark.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright (C) 2020 THL A29 Limited, a Tencent company.
+# All rights reserved.
+# Licensed under the BSD 3-Clause License (the "License"); you may
+# not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+# https://opensource.org/licenses/BSD-3-Clause
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+# See the AUTHORS file for names of contributors.
+
+FILENAME=${0##*/}
+BASENAME=${FILENAME%.*}
+
+export CUBLAS_LOGINFO_DBG=1
+export CUBLAS_LOGDEST_DBG=$PWD/$BASENAME.log
+
+set -e
+FRAMEWORKS=("turbo-transformers" "torch")
+# pip install onnxruntime-gpu before benchmarking onnxrt
+# FRAMEWORKS=("onnxruntime")
+SEQ_LEN=(10 20 40 60 80 100 200 300 400 500)
+BATCH_SIZE=(1 20)
+N=150
+MODEL="bert-base-uncased"
+for batch_size in ${BATCH_SIZE[*]}
+do
+  for seq_len in ${SEQ_LEN[*]}
+  do
+    for framework in ${FRAMEWORKS[*]}
+    do
+      python bertintermidiate_benchmark.py ${MODEL} --seq_len=${seq_len} --batch_size=${batch_size}\
+          -n ${N} --framework=${framework}
+    done
+  done
+done
